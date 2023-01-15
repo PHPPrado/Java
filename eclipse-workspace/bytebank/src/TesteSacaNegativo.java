db8aff3658ff823d7761b5214eb381f40be46eb7
Pedro <pedro.phprado@gmail.com>
2023-01-15 15:50:55 -0300

    Teste

--- eclipse-workspace/bytebank/src/TesteSacaNegativo.java
+++
@@ -1,9 +0,0 @@
-public class TesteSacaNegativo {
-	public static void main(String[] args) {
-		Conta conta = new Conta(3436, 8765);
-		conta.deposita(200);
-		System.out.println(conta.saca(400));
-		System.out.println(conta.getSaldo());
-		
-	}
-}
